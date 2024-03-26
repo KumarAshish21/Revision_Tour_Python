40e5b16d5bf5574cc0d0512a85b157c29b1c07c9
Ashish <AChaurasiya@youngman.co.in>
2024-03-26 16:07:08 +0530

    edit some file

--- Basic/02.py
+++
@@ -1,6 +0,0 @@
-# Check leap year of not?
-year = int(input("Enter the year :"))
-if (year % 4==0) and ((year % 100!=0) or (year % 400 == 0)):
-    print("Leap Year")
-else:
-    print("Not Leap Year")
